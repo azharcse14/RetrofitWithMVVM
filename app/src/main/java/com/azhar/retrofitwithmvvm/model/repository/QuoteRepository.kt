95f703ee426eb4badf6cede8cde3613b33403c38
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2022-01-11 19:41:25 +0600

    update repository

--- app/src/main/java/com/azhar/retrofitwithmvvm/model/repository/QuoteRepository.kt
+++ app/src/main/java/com/azhar/retrofitwithmvvm/model/repository/QuoteRepository.kt
@@ -1,4 +1,20 @@
 package com.azhar.retrofitwithmvvm.model.repository
 
-class QuoteRepository {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.azhar.retrofitwithmvvm.model.api.QuoteService
+import com.azhar.retrofitwithmvvm.model.data.QuoteList
+
+class QuoteRepository(private val quoteService: QuoteService) {
+    private val quotesLiveData = MutableLiveData<QuoteList>()
+
+    val quotes: LiveData<QuoteList>
+        get() =quotesLiveData
+
+    suspend fun getQuotes(page: Int){
+        val result = quoteService.getQuotes(page)
+        if (result?.body()!=null){
+            quotesLiveData.postValue(result.body())
+        }
+    }
 }

dba6d9ec2838392972a5b20175d202caa05a3b43
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2022-01-11 19:47:45 +0600

    Update ViewModel

--- app/src/main/java/com/azhar/retrofitwithmvvm/view_model/MainViewModel.kt
+++ app/src/main/java/com/azhar/retrofitwithmvvm/view_model/MainViewModel.kt
@@ -1,4 +1,21 @@
 package com.azhar.retrofitwithmvvm.view_model
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.azhar.retrofitwithmvvm.model.data.QuoteList
+import com.azhar.retrofitwithmvvm.model.repository.QuoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val repository: QuoteRepository):ViewModel() {
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.getQuotes(1)
+        }
+    }
+
+    val quotes: LiveData<QuoteList>
+        get() = repository.quotes
 }
